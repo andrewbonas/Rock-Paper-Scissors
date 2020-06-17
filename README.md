343af194e0ce7c29b621b7095c8705780d2bdff2
Andrew Bonas <aabonas@hotmail.com>
2020-06-17 12:10:28 -0700

    updated gameplay and CSS styling

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Rock-Paper-Scissors
-intro project to Javascript.
-current project does not include webpage content, only user input provided by prompts.
+Intro project to Javascript.
+
--- script.js
+++ script.js
@@ -1,66 +1,80 @@
 
 let playerScore = 0;
 let computerScore = 0;
+const computer = document.querySelector('#computerScore');
+const player = document.querySelector('#playerScore');
+const result = document.querySelector('#result');
 
-function computerPlay(computerChoice){
-    return computerChoice;
+function computerPlay() {
+    let num = Math.floor(Math.random() * 3) + 1;
+    switch (num) {
+      case 1:
+        return 'rock';
+      case 2:
+        return 'paper';
+      case 3:
+        return 'scissors';
+    }
 }
-    const computerOptions = ['rock', 'paper', 'scissors'];
-    const computerNumber = Math.floor(Math.random() * 3);
-    const computerChoice = computerOptions[computerNumber];
-    
-for(i = 0; i < 3; i++) {
-
 function playRound(playerSelection, computerSelection) {
 	
-    if(playerSelection === computerSelection){
-           alert ('It is a tie! The computer chose ' + computerSelection +".") ; 
-         return;
-         }
-
+    if(playerSelection === computerSelection){ 
+         return ('It is a tie! The computer chose ' + 
+         computerSelection +"."); 
+        }
     if(playerSelection === 'rock'){
         if(computerSelection === 'scissors'){
-            alert('Player Wins! The computer chose ' + computerSelection + ".");
-            return playerScore++;     
-                }else{
-            alert('Computer Wins! The computer chose ' + computerSelection + ".");
-            return computerScore++;
+            playerScore++;
+            return ('Player Wins! The computer chose ' +
+            computerSelection + ".");
+                    }else{
+            computerScore++;
+            return ('Computer Wins! The computer chose ' + 
+                computerSelection + ".");
         }
     }
-
     if(playerSelection === 'paper'){
         if(computerSelection === 'scissors'){
-           alert('Computer Wins! The computer chose ' + computerSelection + ".") ;
-           return computerScore++;      
-                }else{
-            alert('Player Wins! The computer chose ' + computerSelection + ".");
-            return playerScore++;
+            computerScore++;
+           return ('Computer Wins! The computer chose ' + 
+                computerSelection + ".");    
+                    }else{
+                playerScore++;
+            return ('Player Wins! The computer chose ' +
+                computerSelection + ".");
          }
     }
-
      if(playerSelection === 'scissors'){
         if(computerSelection === 'rock'){
-            alert('Computer Wins! The computer chose ' + computerSelection + ".");
-            return computerScore++;     
-                }else{
-            alert('Player Wins! The computer chose ' + computerSelection + ".");
-            return playerScore++;
+            computerScore++;
+            return ('Computer Wins! The computer chose ' + 
+                computerSelection + ".");   
+                    }else{
+             playerScore++;
+            return ('Player Wins! The computer chose ' +
+                computerSelection + ".");
         }
     }
 }
-const playerSelection = prompt("rock, paper or scissors?").toLowerCase();
-const computerSelection = computerPlay(computerChoice)
-console.log(playRound(playerSelection, computerSelection))
-
+function game(){
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            const computerSelection = computerPlay();
+            result.textContent = playRound(button.id, computerSelection)
+            player.textContent = "Player Score: " + playerScore;
+            computer.textContent ="Computer Score: " + computerScore;
+        if (playerScore == 3){
+                result.textContent = "Player Wins Best out of 3!"
+                computerScore = 0;
+                playerScore = 0;
+            }
+        if (computerScore == 3){
+                result.textContent = "Computer Wins Best out of 3!"
+                computerScore = 0;
+                playerScore = 0;
+            }
+        });
+    }); 
 }
-
-alert(`Wins: ${playerScore}, Losses: ${computerScore}`);
-
-if (playerScore === computerScore){
-    alert('It is a tie out of 3!');
-} else if (playerScore < computerScore) {
-    alert('You LOST best out of 3!');
-} else {
-    alert('You WON best out of 3!');
-}
-  
+game();
